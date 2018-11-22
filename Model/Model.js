64b8329cd75f92e2dce1daa7abee27b722802818
Richard Aguirre <richardjap@gmail.com>
2018-11-21 19:11:40 -0500

    error

--- Model/Model.js
+++ Model/Model.js
@@ -1,11 +1,72 @@
-var messageSchema = {
-    titulo:{
+import mongoose from "mongoose";
+import { Schema} from "mongoose";
+
+//model
+ var Modela={  
+      messageSchema : {
+        titulo:{
         type:String,
         required:true
+        },
+        descripcion:String,
+        tipo:String,
+        user:{
+            type:Schema.Types.ObjectId,ref: 'User',
+            required:true
+        }
     },
-    descripcion:String,
-    tipo:String
-}
+     userSchema :{
+        userName:{
+            type:String,
+            required:true
+        },
+        password:{
+            type:String,
+            required:true
+        },
+        fechaCreacio:String,
+        licence:{
+           type:Schema.Types.ObjectId,
+           ref:'Licence'
+        }
+    },
+    licenceSchema:{
+         number:{
+             type:String,
+             required:true
+         },
+         licenceType:{
+             type:Schema.Types.ObjectId,
+             ref:"licenceType"
+         },
+         inicion:String,
+         fin:String
+    },
+    licenceTypeSchema:{
+        name:{
+            type:String,
+            required:true
+        },
+        cost:String,
+        messageCount:Number
+    }
+} ;
+var Schemax={
+    userSchema: new Schema(Modela.userSchema),
+    messageSchema:new Schema(Modela.messageSchema),
+    licenceSchema:new Schema(Modela.licenceSchema),
+    licenceTypeSchema:new Schema(Modela.licenceTypeSchema)
+    }
+//index
+Schemax.userSchema.index({userName:1,licence:1});
+Schemax.messageSchema.index({user:1,titulo:1});
+Schemax.licenceSchema.index({number:1});
+var Modelo={};
+Modelo.User=mongoose.model("User",Schemax.userSchema);
+Modelo.Message=mongoose.model("Message",Schemax.messageSchema);
+Modelo.Licence=mongoose.model("licence",Schemax.licenceSchema);
+Modelo.licenceType=mongoose.model("licenceType",Schemax.licenceTypeSchema);
 
-export default messageSchema;
 
+
+export default {Modelo,mongoose};
--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-import mongoose from 'mongoose';
 import express from 'express';
-import messageSchema from './Model/Model';
+import {Modelo,mongoose} from './Model/Model';
 
 let app=express();
-let Schema=mongoose.Schema;
-
-
-let message_schema=new Schema(messageSchema);
-
-var Message=mongoose.model("Message",message_schema);
+var Modelx=Modelo;
+var Message=Modelo.Message;
 app.get("/",(req,res)=>{
     res.send('Hola Mongoose');
 });
@@ -43,7 +38,7 @@ app.post('/message',express.json(),(req,resp)=>{
 
 mongoose.connect('mongodb://localhost:27017/QuiclyMessages')
 .then(()=>console.log('Mongo Connected'))
-.catch(()=>console.log('Mongo Error'));
+.catch((err)=>console.log(err));
 
 app.listen('3000',()=> console.log('express running in PORT 3000'));
 
